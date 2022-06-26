60fce73452ef9556a4a17d8f36a4bf5152a5dba6
czhenhao <czhenhao@hotmail.com>
2022-06-27 02:07:41 +0800

    added more console.logs to authjs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
+  console.log(`auth.js ${req.session}`);
   console.log(`auth.js ${req.session.userId}`);
+  console.log(`auth.js ${req.session.currentUser}`);
   if (req.session.userId) {
+    console.log(`logged in as ${req.session.currentUser}`);
     next();
   } else {
     res.status(403).json({ title: "error", message: "not logged in" });
